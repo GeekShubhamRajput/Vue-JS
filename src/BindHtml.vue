aa99eba872f20d4a825a722c4663d38441f5eaa1
Shubham Rajput <rajput.shubh1993@gmail.com>
2020-12-16 23:12:43 +0530

    Bind HTML only

---
+++ src/BindHtml.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-html="technologies"> </div>
+  <p v-html="myLink"> </p>
+</template>
+
+<script>
+  export default{
+    name: "BindHtml",
+    data(){
+      return{
+        technologies: '<b>Ruby on Rails</b>',
+        myLink: `<a herf="#" onclick="alert('Congratulation !!')">Click here to win prize</a>`,
+      };
+    },
+  };
+</script>
