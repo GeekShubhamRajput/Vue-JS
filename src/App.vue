d7423318cbfd4159216eafc4a7efa5787a19a2da
Shubham Rajput <rajput.shubh1993@gmail.com>
2020-12-16 23:04:23 +0530

    Binding HTML III

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div> My name is {{ name }} </div>
   <div> I am {{ designation }} </div>
-  <div v-text="technologies"> </div>
+  <div v-html="technologies"> </div>
+  <p v-html="myLink"> </p>
 </template>
 
 <script>
-
   export default{
     name: "App",
     data(){
       return{
         name: "Shubham Rajput",
         designation: "Software Engineer",
-        technologies: 'Ruby on Rails',
+        technologies: '<b>Ruby on Rails</b>',
+        myLink: `<a herf="#" onclick="alert('Congratulation !!')">Click here to win prize</a>`,
       };
     },
   };
-
 </script>
