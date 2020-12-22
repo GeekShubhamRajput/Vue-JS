b1a7e725339e48f4921acbb4763efd0475fb4c8d
Shubham Rajput <rajput.shubh1993@gmail.com>
2020-12-22 22:58:56 +0530

    Conditional rendering part 1

---
+++ src/ConditionalRendering.vue
@@ -0,0 +1,28 @@
+<template>
+  <p v-if="num===0">Number is zero</p>
+  <p v-else-if="num > 0">Number is Positive</p>
+  <p v-else-if="num < 0">Number is Negative</p>
+  <p v-else>This is not a number</p>
+
+  <div v-if="display">
+    <p>My name is Shubham Rajput</p>
+  </div>
+</template>
+
+<script>
+  export default{ 
+    data(){
+      return{
+        num: 1,
+        display: true,
+      }
+    }
+  }
+</script>
+
+<style>
+  #app{
+    text-align: center;
+    font-size: 35px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './ShortHands.vue'
+import App from './ConditionalRendering.vue'
 
 createApp(App).mount('#app')
