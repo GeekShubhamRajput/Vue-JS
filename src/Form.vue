ba57091fbd23216d64b305c46234a2fa585785aa
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-01-01 00:09:59 +0530

    Form Handling

---
+++ src/Form.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <pre>
+      {{ JSON.stringify(formValues, null, 2) }}
+    </pre>
+  </div>
+
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="formValues.name" />
+    </div>
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea id="profile" v-model="formValues.profileSummary" />
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+    <div>
+      <label for="job-location">Job location</label>
+      <select id="job-location" v-model="formValues.jobLocation" multiple>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+  </form>
+</template>
+
+<script>
+  export default{
+    name: 'Form',
+    data(){
+      return{
+        formValues: {
+          name: '',
+          profileSummary: '',
+          country: '',
+          jobLocation: []
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+    margin-left: 15px;
+  }
+  
+  label {
+    font-weight: bold;
+    display: flex;
+    margin-bottom: 5px;
+    margin-top: 10px;
+  }
+
+  input + label {
+    font-weight: bold;
+    display: inline-flex;
+    margin-right: 20px;
+  }
+
+  input[type='text'],
+  textarea,
+  select {
+    display: block;
+    width: 400px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #555;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+</style>
