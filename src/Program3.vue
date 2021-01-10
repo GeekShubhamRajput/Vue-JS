ba1e4a065169aa052a4f05c6f6ca3e10efbb63c0
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-01-10 17:43:30 +0530

    Computed property program

---
+++ src/Program3.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Has Books: {{hasBooks}}</h2>
+</template>
+
+<script>
+  export default{
+    name: 'ComputedProperty',
+    data(){
+      return{
+        authorBook: {
+          author: 'Shubham',
+          books: [
+            'Alchemist', 'The Heror', 'KingMan'
+          ]
+        }
+      }
+    },
+    computed: {
+      hasBooks(){
+        return this.authorBook.books.length > 0 ? 'Yes' : 'No';
+      }
+    }
+  }
+</script>
+
+<style>
+  #app{
+    text-align: center;
+    color: blue;
+  }
+</style>
