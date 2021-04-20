ba5be455a1a393cd6b6cc34fc88a83f7e7c1915b
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-04-20 20:34:03 +0530

    Implement re-use of components

---
+++ vue-components/src/components/Greetings1.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{ name}} a.k.a. {{ heroName}}</h2>
+</template>
+
+<script>
+  export default{
+    name: 'Greetings',
+    props: ['name', 'heroName']
+  }
+</script>
+
+<style>
+</style>
