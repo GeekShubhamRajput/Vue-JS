f51270a5ae3a0aae3beeaf4ae982426bb59b8e71
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-05-02 20:14:55 +0530

    HTTP GET Request

--- vue-http/src/App.vue
+++ vue-http/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>HTTP GET Request</h1>
+  <PostList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import PostList from './components/PostList.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      PostList,
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ vue-http/src/components/PostList.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click = "getPostList()"> Load Post</button>
+  <h2 v-if="errorMsg" >{{ errorMsg}}</h2>
+  <div v-for="post in posts" :key="post.id" >
+    <h3>{{post.id}}. {{ post.title}}</h3>
+    <p>{{ post.body}}</p>
+    <hr />
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default{
+    name: "PostList",
+    data(){
+      return{
+        posts: [],
+        errorMsg: '',
+      }
+    },
+    methods: {
+      getPostList(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+          return this.posts = response.data;
+        }).catch((error) => {
+          console.log(error)
+          return this.errorMsg = "Something went wrong..."
+        })
+      } 
+    }
+  }
+</script>
+
+<style></style>
